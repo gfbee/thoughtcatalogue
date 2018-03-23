f50fe61b17b97a612de074f32b9a3f48a86a5835
gfbee <gfb@cs.toronto.edu>
2018-03-23 18:48:21 -0400

    Safer clearly scoped file I/O

--- core/nodejstest/simple.py
+++ core/nodejstest/simple.py
@@ -1,7 +1,6 @@
 import sys
 
 print("Attempting to print")
-fileP = open("newfile", "w")
-fileP.write("Does this work?")
-fileP.write("Are we sure?")
-fileP.close()
+with open("newfile", "w") as fileP:
+    fileP.write("Does this work?")
+    fileP.write("Are we sure?")
